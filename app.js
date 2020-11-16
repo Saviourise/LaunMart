ea7af7ea0dbc99eea064b9da47437336098c4c7b
Saviourise <65181992+Saviourise@users.noreply.github.com>
2020-11-16 12:10:32 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,26 @@
+window.onload = () => {
+    //document.querySelector("i").style.display = "none"
+    n = 0
+  /*  while(n<=8) {
+        n += 1
+        if (n == 6) {
+            document.querySelector("#loader").style.visibility = "hidden"
+            break;
+        }
+        
+    }*/
+    setTimeout(() => {
+        
+            document.querySelector("#loader").style.display = "none"
+            
+           /* var btn = document.querySelector("button")
+            btn.style.display = "block"*/
+            window.location.replace("login.html");
+    },12000)
+    
+    
+   /* btn.addEventListener("click", (e) => {
+        e.preventDefault()
+    })*/
+    
+}
